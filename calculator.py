2b0f42fe68ff2a5b906a7c8060e585d618291055
vagrant <vagrant@vagrant.vm>
2020-09-30 21:07:54 +0000

    Fixes IndexError for square and cube functions and evaluates input for mod function

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-"""CLI application for a prefix-notation calculator."""
+
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
@@ -7,25 +7,30 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 def prefix_notation_calculator():
-    """Take in an operation and 2 numbers and perform the operation """
+    """CLI application for a prefix-notation calculator."""
+    try:
+    
 
-    operation_input[1] = float(operation_input[1])
-    operation_input[2] = float(operation_input[2])
-    if operation_input[0] == "+":
-        return add(operation_input[1], operation_input[2])
-    elif operation_input[0] == "-":
-        return subtract(operation_input[1], operation_input[2])
-    elif operation_input[0] == "*":
-        return multiply(operation_input[1], operation_input[2])
-    elif operation_input[0] == "/":
-        return divide(operation_input[1], operation_input[2])
-    elif operation_input[0] == "square":
-        return square(operation_input[1], operation_input[2])
-    elif operation_input[0] == "cube":
-        return cube(operation_input[1], operation_input[2])
-    elif operation_input[0] == "pow":
-        return power(operation_input[1], operation_input[2])
-
+        operation_input[1] = float(operation_input[1])
+        operation_input[2] = float(operation_input[2])
+        if operation_input[0] == "+":
+            return add(operation_input[1], operation_input[2])
+        elif operation_input[0] == "-":
+            return subtract(operation_input[1], operation_input[2])
+        elif operation_input[0] == "*":
+            return multiply(operation_input[1], operation_input[2])
+        elif operation_input[0] == "/":
+            return divide(operation_input[1], operation_input[2])
+        elif operation_input[0] == "pow":
+            return power(operation_input[1], operation_input[2])
+        elif operation_input[0] == "mod":
+            return mod(operation_input[1], operation_input[2])
+    except IndexError:
+        operation_input[1] = float(operation_input[1])
+        if operation_input[0] == "square":
+            return square(operation_input[1])
+        elif operation_input[0] == "cube":
+            return cube(operation_input[1])
 while True:
     user_calculation = input()
     # read input
