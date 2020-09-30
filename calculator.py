5e1cb5839b69fefb9174370ca770643cdbfb9914
vagrant <vagrant@vagrant.vm>
2020-09-30 20:32:42 +0000

    Creates REPL and Evaluates Add and Subtract

--- calculator.py
+++ calculator.py
@@ -4,4 +4,29 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
+
+
+def prefix_notation_calculator():
+    """Take in an operation and 2 numbers and perform the operation """
+
+    operation_input[1] = float(operation_input[1])
+    operation_input[2] = float(operation_input[2])
+    if operation_input[0] == "+":
+        return add(operation_input[1], operation_input[2])
+    elif operation_input[0] == "-":
+        return subtract(operation_input[1], operation_input[2])  
+
+
+while True:
+    user_calculation = input()
+    # read input
+    # tokenize input
+    operation_input = user_calculation.split()
+   
+    #     if the first token is "q":
+
+    if operation_input[0] == "q":
+        break  
+    else:
+        print(prefix_notation_calculator())
 # Replace this with your code
